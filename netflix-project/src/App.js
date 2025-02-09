133d0ee975524a08b7c1e0db4e90fc42167e3c80
teketel <tekch04@gmail.com>
2025-02-09 13:06:28 +0300

    header and footer

--- netflix-project/src/App.js
+++ netflix-project/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
-    </div>
+    <>
+     <Header/>
+     <Footer/>
+      
+    </>
   );
 }
 
